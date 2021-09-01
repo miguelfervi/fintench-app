334c08d5eddc25909405b23ffca545fa79372ad3
Miguel <miguelfervi@gmail.com>
2021-09-01 22:59:38 +0200

    ADD redux to the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+
+import { store } from "./store/store";
 
 function App() {
   return (
-    <Layout>
-    <Switch>
-      <Route path='/' exact>
-        <HomePage />
-      </Route>
-      <Route path='/auth'>
-        <AuthPage />
-      </Route>
-      <Route path='/profile'>
-        <UserProfile />
-      </Route>
-    </Switch>
-  </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+          <Route path="/profile">
+            <UserProfile />
+          </Route>
+        </Switch>
+      </Layout>
+    </Provider>
   );
 }
 
