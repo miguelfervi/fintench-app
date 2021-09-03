6cf98e0bf7d7a8b5c12a0ea47e98fcba2b59311b
Miguel <miguelfervi@gmail.com>
2021-09-03 20:16:48 +0200

    Add unit test for StartingPageContent

--- src/components/StartingPage/__tests__/StartingPageContent.test.js
+++ src/components/StartingPage/__tests__/StartingPageContent.test.js
@@ -1,8 +1,25 @@
-import React from 'react';import { render, screen } from '@testing-library/react';
-import StartingPageContent from '../StartingPageContent';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import StartingPageContent from "../StartingPageContent";
 
-test('renders learn react link', () => {
+test("renders component StartingPageContent", () => {
   render(<StartingPageContent />);
-  const linkElement = screen.getByText(/Welcome to Fintech App!/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Welcome to Fintech App!/i);
+  expect(titleElement).toBeInTheDocument();
+  const subTitleElement = screen.getByText(
+    /click on Log In and sign in or sign up/i
+  );
+  expect(subTitleElement).toBeInTheDocument();
+});
+
+test("test content StartingPageContent", () => {
+  render(<StartingPageContent />);
+  const titleElement = screen.getByText(/Welcome to Fintech App!/i);
+  const subTitleElement = screen.getByText(
+    /click on Log In and sign in or sign up/i
+  );
+  expect(titleElement.textContent).toEqual("Welcome to Fintech App!");
+  expect(subTitleElement.textContent).toEqual(
+    "To start, click on Log In and sign in or sign up"
+  );
 });
