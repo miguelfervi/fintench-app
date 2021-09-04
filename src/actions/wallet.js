f0ec71366c27cc6ba3facbd3df0494f9124da19d
Miguel <miguelfervi@gmail.com>
2021-09-04 17:02:47 +0200

    Fix issue

--- src/actions/wallet.js
+++ src/actions/wallet.js
@@ -25,7 +25,7 @@ export const retireBalance = (value) => {
 export const addTransaction = (value, name, id) => {
   let text = "";
   if (value < 0 && name === '') {
-    text = `You have retired from the wallet $${value}`;
+    text = `You have retired from the wallet $${ -1* value}`;
   } else if (value > 0 && name === "") {
     text = `You have entered from the wallet $${value}`;
   } else {
